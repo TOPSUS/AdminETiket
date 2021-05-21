f1266cb8af44178d145baff83d0955717c7238cb
sintlort <61153093+sintlort@users.noreply.github.com>
2021-05-21 22:27:09 +0800

    try2

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        RealRashid\SweetAlert\SweetAlertServiceProvider::class,
 
     ],
 
